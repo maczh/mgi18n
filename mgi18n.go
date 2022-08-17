9739f6a7cf1545caf1f74cacc0e8c090153a3e7b
maczh <Micro!070804>
2022-08-18 02:08:14 +0800

    first commit

---
+++ mgi18n.go
@@ -0,0 +1,120 @@
+package mgi18n
+
+import (
+	"fmt"
+	"github.com/maczh/gintool/mgresult"
+	"github.com/maczh/logs"
+	"github.com/maczh/mgcache"
+	"github.com/maczh/mgconfig"
+	"github.com/maczh/mgerr"
+	"github.com/maczh/mgi18n/xlang"
+	"time"
+)
+
+var appName, defaultLanguage string
+
+func Init() {
+	appName = mgconfig.GetConfigString("go.xlang.appName")
+	if appName == "" {
+		appName = mgconfig.GetConfigString("go.application.name")
+	}
+	defaultLanguage = mgconfig.GetConfigString("go.xlang.default")
+	if defaultLanguage == "" {
+		defaultLanguage = "zh-cn"
+	}
+	version, err := xlang.GetAppXLangVersion(appName)
+	if err != nil {
+		logs.Error(err.Error())
+		return
+	}
+	initCache(version)
+	//设置定时任务自动检查
+	ticker := time.NewTicker(time.Minute * 5)
+	go func() {
+		for _ = range ticker.C {
+			reflushXLangCache()
+		}
+	}()
+}
+
+func initCache(version string) {
+	mgcache.OnGetCache("x-lang").Add("version", version, 0)
+	langs, err := xlang.GetAppXLangStringsAll(appName)
+	if err != nil {
+		logs.Error(err.Error())
+		return
+	}
+	for k,v := range langs {
+		mgcache.OnGetCache("x-lang").Add(k,v, 0)
+	}
+	mgcache.OnGetCache("x-lang").Add("success:zh-cn","成功",0)
+	mgcache.OnGetCache("x-lang").Add("success:en-us","Success",0)
+	mgcache.OnGetCache("x-lang").Add("success:zh-tw","成功",0)
+	mgcache.OnGetCache("x-lang").Add("success:ja","成功",0)
+	mgcache.OnGetCache("x-lang").Add("success:fr","Succès",0)
+	mgcache.OnGetCache("x-lang").Add("success:it","Successo",0)
+	mgcache.OnGetCache("x-lang").Add("success:de","der Erfolg",0)
+	mgcache.OnGetCache("x-lang").Add("success:ko","성공",0)
+}
+
+func GetXLangString(stringId, lang string) string {
+	key := fmt.Sprintf("%s:%s", stringId, lang)
+	str, ok := mgcache.OnGetCache("x-lang").Value(key)
+	if ok {
+		return str.(string)
+	}
+	key = fmt.Sprintf("%s:%s", stringId, defaultLanguage)
+	str, ok = mgcache.OnGetCache("x-lang").Value(key)
+	if ok {
+		return str.(string)
+	}
+	return ""
+}
+
+func reflushXLangCache() {
+	version, err := xlang.GetAppXLangVersion(appName)
+	if err != nil {
+		logs.Error(err.Error())
+		return
+	}
+	oldVersion, ok := mgcache.OnGetCache("x-lang").Value("version")
+	if ok {
+		if oldVersion != version {
+			mgcache.OnGetCache("x-lang").Clear()
+			initCache(version)
+		}
+	}
+}
+
+
+func ErrorXLang(code int, messageId string) mgresult.Result {
+	return mgresult.Error(code, String(messageId))
+}
+
+func ErrorXLangWithMsg(code int, messageId, msg string) mgresult.Result {
+	return mgresult.Error(code, fmt.Sprintf("%s:%s", String(messageId), msg))
+}
+
+func Success(data interface{}) mgresult.Result {
+	return mgresult.SuccessWithMsg(String("success"), data)
+}
+
+func SuccessWithPage(data interface{},count, index, size, total int) mgresult.Result {
+	return mgresult.Result{
+		Status: 1,
+		Msg:    String("success"),
+		Data:   data,
+		Page:   &mgresult.ResultPage{
+			Count: count,
+			Index: index,
+			Size:  size,
+			Total: total,
+		},
+	}
+}
+
+
+func String(messageId string) string{
+	lang := mgerr.GetCurrentLanguage()
+	return  GetXLangString(messageId, lang)
+}
---
+++ xlang/x-lang.go
@@ -0,0 +1,61 @@
+package xlang
+
+import (
+	"errors"
+	"github.com/maczh/gintool/mgresult"
+	"github.com/maczh/logs"
+	"github.com/maczh/mgcall"
+	"github.com/maczh/utils"
+	"gopkg.in/mgo.v2/bson"
+)
+
+const (
+	SERVICE_X_LANGUAGE = "x-lang"
+	URI_LIST_STRINGS_BY_APPNAME = "/lang/string/list"
+	URI_GET_APP_STRINGS_VERSION = "/lang/string/app/version"
+)
+
+type LangInfo struct {
+	Id       bson.ObjectId `json:"id" bson:"_id"`
+	StringId string        `json:"stringId" bson:"stringId"`
+	AppName  string        `json:"appName" bson:"appName"`
+	Lang     string        `json:"lang" bson:"lang"`
+	Text     string        `json:"text" bson:"text"`
+}
+
+func mgCall(service, uri string, params map[string]string) mgresult.Result {
+	res, err := mgcall.Call(service, uri, params)
+	if err != nil {
+		logs.Error("微服务{}{}调用异常:{}", service, uri, err.Error())
+		return mgresult.Error(-1,"xlang service unavailable")
+	}
+	var result mgresult.Result
+	utils.FromJSON(res, &result)
+	return result
+}
+
+func GetAppXLangVersion(appName string) (string,error) {
+	params := map[string]string{
+		"appName": appName,
+	}
+	rs := mgCall(SERVICE_X_LANGUAGE,URI_GET_APP_STRINGS_VERSION,params)
+	if rs.Status != 1 {
+		return "",errors.New(rs.Msg)
+	}
+	data := make(map[string]string)
+	utils.FromJSON(utils.ToJSON(rs.Data), &data)
+	return data["version"],nil
+}
+
+func GetAppXLangStringsAll(appName string) (map[string]string,error) {
+	params := map[string]string{
+		"appName": appName,
+	}
+	rs := mgCall(SERVICE_X_LANGUAGE,URI_LIST_STRINGS_BY_APPNAME,params)
+	if rs.Status != 1 {
+		return nil,errors.New(rs.Msg)
+	}
+	result := make(map[string]string)
+	utils.FromJSON(utils.ToJSON(rs.Data), &result)
+	return result,nil
+}
